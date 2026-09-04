edbb76b082da02ea5169220d8ee5436fad55ef33
a7b3c5d6e6f8g2h1i2j9 <anirudhananu592@gmail.com>
2026-09-04 12:46:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,216 @@
+
+
+// ACCOUNT TOTAL AMOUNT DISPLAY
+
+let accountAmount = '2,100/-';
+
+const accountAmountDisplay = document.getElementById('accountAmountDisplay');
+
+accountAmountDisplay.textContent = `Account : ₹${accountAmount}`;
+
+
+//HIGHLIGHT DATE DISPLAY
+const highlightDateDisplay = document.getElementById('highlightDateDisplay');
+
+highlightDateDisplay.textContent = `Sep : 5/9/26 - Saturday Fund`;
+highlightDateDisplay.style.color = 'var(--highlighttextcolor)';
+
+
+
+const idOne_Adithyan_WeekFund = 0; 
+const idTwo_Jishnu_WeekFund = 0; 
+const idThree_Nishanth_WeekFund = 0; 
+const idFour_Anirudhan_WeekFund = 0; 
+const idFive_Vasu_WeekFund = 0; 
+const idSix_Midhun_WeekFund = 0; 
+const idSeven_Nkhil_WeekFund = 0; 
+const idEight_Jamshika_WeekFund = 0; 
+const idNine_Amika_WeekFund = 0; 
+const idTen_SasiEttan_WeekFund = 0; 
+const idEleven_Krishnaja_WeekFund = 0; 
+const idTwelve_Vishnuja_WeekFund = 0; 
+const idThirteen_Malu_WeekFund = 0; 
+
+
+
+// ID ONE 1 ADITHYAN
+const idOneName = document.getElementById('idOneName');
+const idOneFund = document.getElementById('idOneFund'); 
+
+idOneName.textContent = 'Adithyan';
+
+if(idOne_Adithyan_WeekFund) {
+  idOneFund.textContent = `${idOne_Adithyan_WeekFund}`;
+} else {
+  idOneFund.textContent = 'Not Paid';
+  idOneFund.style.color = 'red';
+}
+
+
+// ID TWO 2 JISHNU
+const idTwoName = document.getElementById('idTwoName');
+const idTwoFund = document.getElementById('idTwoFund');
+
+idTwoName.textContent = 'Jishnu';
+
+if(idTwo_Jishnu_WeekFund) {
+  idTwoFund.textContent = `${idTwo_Jishnu_WeekFund}`;
+} else {
+  idTwoFund.textContent = 'Not Paid';
+  idTwoFund.style.color = 'red';
+}
+
+
+// ID THREE 3 NISHANTH
+const idThreeName = document.getElementById('idThreeName');
+const idThreeFund = document.getElementById('idThreeFund');
+
+idThreeName.textContent = 'Nishanth';
+
+if(idThree_Nishanth_WeekFund) {
+  idThreeFund.textContent = `${idThree_Nishanth_WeekFund}`;
+} else {
+  idThreeFund.textContent = 'Not Paid';
+  idThreeFund.style.color = 'red';
+}
+
+
+// ID FOUR 4 ANIRUDHAN
+const idFourName = document.getElementById('idFourName');
+const idFourFund = document.getElementById('idFourFund');
+
+idFourName.textContent = 'Anirudhan';
+
+if(idFour_Anirudhan_WeekFund) {
+  idFourFund.textContent = `${idFour_Anirudhan_WeekFund}`;
+} else {
+  idFourFund.textContent = 'Not Paid';
+  idFourFund.style.color = 'red';
+}
+
+
+// ID FIVE 5 VASU
+const idFiveName = document.getElementById('idFiveName');
+const idFiveFund = document.getElementById('idFiveFund');
+
+idFiveName.textContent = 'vasu';
+
+if(idFive_Vasu_WeekFund) {
+  idFiveFund.textContent = `${idFive_Vasu_WeekFund}`;
+} else {
+  idFiveFund.textContent = 'Not Paid';
+  idFiveFund.style.color = 'red';
+}
+
+
+// ID SIX 6 MIDHUN
+const idSixName = document.getElementById('idSixName');
+const idSixFund = document.getElementById('idSixFund');
+
+idSixName.textContent = 'Midhun';
+
+if(idSix_Midhun_WeekFund) {
+  idSixFund.textContent = `${idSix_Midhun_WeekFund}`;
+} else {
+  idSixFund.textContent = 'Not Paid';
+  idSixFund.style.color = 'red';
+}
+
+
+// ID SEVEN 7 NIKHIL
+const idSevenName = document.getElementById('idSevenName');
+const idSevenFund = document.getElementById('idSevenFund');
+
+idSevenName.textContent = 'Nikhil';
+
+if(idSeven_Nkhil_WeekFund) {
+  idSevenFund.textContent = `${idSeven_Nkhil_WeekFund}`;
+} else {
+  idSevenFund.textContent = 'Not Paid';
+  idSevenFund.style.color = 'red';
+}
+
+
+// ID EIGHT 8 JAMSHIKA
+const idEightName = document.getElementById('idEightName');
+const idEightFund = document.getElementById('idEightFund');
+
+idEightName.textContent = 'Jamshika';
+
+if(idEight_Jamshika_WeekFund) {
+  idEightFund.textContent = `${idEight_Jamshika_WeekFund}`;
+} else {
+  idEightFund.textContent = 'Not Paid';
+  idEightFund.style.color = 'red';
+}
+
+
+// ID NINE 9 AMIKA
+const idNineName = document.getElementById('idNineName');
+const idNineFund = document.getElementById('idNineFund');
+
+idNineName.textContent = 'Amika';
+
+if(idNine_Amika_WeekFund) {
+  idNineFund.textContent = `${idNine_Amika_WeekFund}`;
+} else {
+  idNineFund.textContent = 'Not Paid';
+  idNineFund.style.color = 'red';
+}
+
+
+// ID TEN 10 SASI ETTAN
+const idTenName = document.getElementById('idTenName');
+const idTenFund = document.getElementById('idTenFund');
+
+idTenName.textContent = 'Sasi Ettan';
+
+if(idTen_SasiEttan_WeekFund) {
+  idTenFund.textContent = `${idTen_SasiEttan_WeekFund}`;
+} else {
+  idTenFund.textContent = 'Not Paid';
+  idTenFund.style.color = 'red';
+}
+
+
+// ID ELEVEN 11 KRISHNAJA
+const idElevenName = document.getElementById('idElevenName');
+const idElevenFund = document.getElementById('idElevenFund');
+
+idElevenName.textContent = 'Krishnaja';
+
+if(idEleven_Krishnaja_WeekFund) {
+  idElevenFund.textContent = `${idEleven_Krishnaja_WeekFund}`;
+} else {
+  idElevenFund.textContent = 'Not Paid';
+  idElevenFund.style.color = 'red';
+}
+
+
+// ID TWELVE 12 VISHNUJA
+const idTwelveName = document.getElementById('idTwelveName');
+const idTwelveFund = document.getElementById('idTwelveFund');
+
+idTwelveName.textContent = 'Vishnuja';
+
+if(idTwelve_Vishnuja_WeekFund) {
+  idTwelveFund.textContent = `${idTwelve_Vishnuja_WeekFund}`;
+} else {
+  idTwelveFund.textContent = 'Not Paid';
+  idTwelveFund.style.color = 'red';
+}
+
+
+// ID THIRTEEN 13 MALU
+const idThirteenName = document.getElementById('idThirteenName');
+const idThirteenFund = document.getElementById('idThirteenFund');
+
+idThirteenName.textContent = 'Malu';
+
+if(idThirteen_Malu_WeekFund) {
+  idThirteenFund.textContent = `${idThirteen_Malu_WeekFund}`;
+} else {
+  idThirteenFund.textContent = 'Not Paid';
+  idThirteenFund.style.color = 'red';
+}
+
---
+++ shortage.js
@@ -0,0 +1,290 @@
+
+
+//SHORTAGE PAGE
+
+const totalAmount =  1700; //TYPE HERE THE TOTAL FUND
+
+const totalAmountDisplay = document.getElementById('totalAmountDisplay');
+ 
+
+totalAmountDisplay.textContent = ` Total Amount : ${totalAmount}/-`;
+
+
+//HIGHLIGHT TEXT DISPLAY
+const highlightTextDisplay = document.getElementById('highlightTextDisplay');
+
+highlightTextDisplay.textContent = `This is the amount you need to pay`;
+highlightTextDisplay.style.color = 'var(--highlighttextcolor)';
+
+
+//TOTAL FUND THAT THE USERS PAY
+const idOne_Adithyan_TotalFund = 200;
+const idTwo_Jishnu_TotalFund = 0;
+const idThree_Nishanth_TotalFund = 200;
+const idFour_Anirudhan_TotalFund = 100;
+const idFive_Vasu_TotalFund = 0;
+const idSix_Midhun_TotalFund = 0;
+const idSeven_Nikhil_TotalFund = 0;
+const idEight_Jamshika_TotalFund = 1200;
+const idNine_Amika_TotalFund = 0;
+const idTen_SasiEttan_TotalFund = 100;
+const idEleven_Krishnaja_TotalFund = 100;
+const idTwelve_Vishnuja_TotalFund = 100;
+const idThirteen_Malu_TotalFund = 100;
+
+
+
+
+// ID ONE 1 ADITHYAN
+const idOneName = document.getElementById('idOneName');
+const idOneShort = document.getElementById('idOneShort'); 
+
+idOneName.textContent = 'Adithyan';
+
+const idOneShortCalc = totalAmount - idOne_Adithyan_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idOne_Adithyan_TotalFund === totalAmount) {
+
+  idOneShort.textContent = `${idOne_Adithyan_TotalFund}/- Full Paid`;
+  idOneShort.style.color = 'darkcyan';
+
+} else {
+  idOneShort.textContent = `${idOneShortCalc}/-`;
+  idOneShort.style.color = 'red';
+}
+
+
+
+// ID TWO 2 JISHNU
+const idTwoName = document.getElementById('idTwoName');
+const idTwoShort = document.getElementById('idTwoShort'); 
+
+idTwoName.textContent = 'Jishnu';
+
+const idTwoShortCalc = totalAmount - idTwo_Jishnu_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idTwo_Jishnu_TotalFund === totalAmount) {
+
+  idTwoShort.textContent = `${idTwo_Jishnu_TotalFund}/- Full Paid`;
+  idTwoShort.style.color = 'darkcyan';
+
+} else {
+  idTwoShort.textContent = `${idTwoShortCalc}/-`;
+  idTwoShort.style.color = 'red';
+}
+
+
+// ID THREE 3 NISHANTH
+const idThreeName = document.getElementById('idThreeName');
+const idThreeShort = document.getElementById('idThreeShort'); 
+
+idThreeName.textContent = 'Nishanth';
+
+const idThreeShortCalc = totalAmount - idThree_Nishanth_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idThree_Nishanth_TotalFund === totalAmount) {
+
+  idThreeShort.textContent = `${idThree_Nishanth_TotalFund}/- Full Paid`;
+  idThreeShort.style.color = 'darkcyan';
+
+} else {
+  idThreeShort.textContent = `${idThreeShortCalc}/-`;
+  idThreeShort.style.color = 'red';
+}
+
+
+// ID FOUR 4 ANIRUDHAN
+const idFourName = document.getElementById('idFourName');
+const idFourShort = document.getElementById('idFourShort'); 
+
+idFourName.textContent = 'Anirudhan';
+
+const idFourShortCalc = totalAmount - idFour_Anirudhan_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idFour_Anirudhan_TotalFund === totalAmount) {
+
+  idFourShort.textContent = `${idFour_Anirudhan_TotalFund}/- Full Paid`;
+  idFourShort.style.color = 'darkcyan';
+
+} else {
+  idFourShort.textContent = `${idFourShortCalc}/-`;
+  idFourShort.style.color = 'red';
+}
+
+
+// ID FIVE 5 VASU
+const idFiveName = document.getElementById('idFiveName');
+const idFiveShort = document.getElementById('idFiveShort'); 
+
+idFiveName.textContent = 'Vasu';
+
+const idFiveShortCalc = totalAmount - idFive_Vasu_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idFive_Vasu_TotalFund === totalAmount) {
+
+  idFiveShort.textContent = `${idFive_Vasu_TotalFund}/- Full Paid`;
+  idFiveShort.style.color = 'darkcyan';
+
+} else {
+  idFiveShort.textContent = `${idFiveShortCalc}/-`;
+  idFiveShort.style.color = 'red';
+}
+
+
+// ID SIX 6 MIDHUN
+const idSixName = document.getElementById('idSixName');
+const idSixShort = document.getElementById('idSixShort'); 
+
+idSixName.textContent = 'Midhun';
+
+const idSixShortCalc = totalAmount - idSix_Midhun_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idSix_Midhun_TotalFund === totalAmount) {
+
+  idSixShort.textContent = `${idSix_Midhun_TotalFund}/- Full Paid`;
+  idSixShort.style.color = 'darkcyan';
+
+} else {
+  idSixShort.textContent = `${idSixShortCalc}/-`;
+  idSixShort.style.color = 'red';
+}
+
+
+
+// ID SEVEN 7 NIKHIL
+const idSevenName = document.getElementById('idSevenName');
+const idSevenShort = document.getElementById('idSevenShort'); 
+
+idSevenName.textContent = 'Nikhil';
+
+const idSevenShortCalc = totalAmount - idSeven_Nikhil_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idSeven_Nikhil_TotalFund === totalAmount) {
+
+  idSevenShort.textContent = `${idSeven_Nikhil_TotalFund}/- Full Paid`;
+  idSevenShort.style.color = 'darkcyan';
+
+} else {
+  idSevenShort.textContent = `${idSevenShortCalc}/-`;
+  idSevenShort.style.color = 'red';
+}
+
+
+
+// ID EIGHT 8 JAMSHIKA
+const idEightName = document.getElementById('idEightName');
+const idEightShort = document.getElementById('idEightShort'); 
+
+idEightName.textContent = 'Jamshika';
+
+const idEightShortCalc = totalAmount - idEight_Jamshika_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idEight_Jamshika_TotalFund === totalAmount) {
+
+  idEightShort.textContent = `${idEight_Jamshika_TotalFund}/- Full Paid`;
+  idEightShort.style.color = 'darkcyan';
+
+} else {
+  idEightShort.textContent = `${idEightShortCalc}/-`;
+  idEightShort.style.color = 'red';
+}
+
+
+
+// ID NINE 9 AMIKA
+const idNineName = document.getElementById('idNineName');
+const idNineShort = document.getElementById('idNineShort'); 
+
+idNineName.textContent = 'Amika';
+
+const idNineShortCalc = totalAmount - idNine_Amika_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idNine_Amika_TotalFund === totalAmount) {
+
+  idNineShort.textContent = `${idNine_Amika_TotalFund}/- Full Paid`;
+  idNineShort.style.color = 'darkcyan';
+
+} else {
+  idNineShort.textContent = `${idNineShortCalc}/-`;
+  idNineShort.style.color = 'red';
+}
+
+
+
+// ID TEN 10 SASI ETTAN
+const idTenName = document.getElementById('idTenName');
+const idTenShort = document.getElementById('idTenShort'); 
+
+idTenName.textContent = 'Sasi Ettan';
+
+const idTenShortCalc = totalAmount - idTen_SasiEttan_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idTen_SasiEttan_TotalFund === totalAmount) {
+
+  idTenShort.textContent = `${idTen_SasiEttan_TotalFund}/- Full Paid`;
+  idTenShort.style.color = 'darkcyan';
+
+} else {
+  idTenShort.textContent = `${idTenShortCalc}/-`;
+  idTenShort.style.color = 'red';
+}
+
+
+
+// ID ELEVEN 11 KRISHNAJA
+const idElevenName = document.getElementById('idElevenName');
+const idElevenShort = document.getElementById('idElevenShort'); 
+
+idElevenName.textContent = 'Krishnaja';
+
+const idElevenShortCalc = totalAmount - idEleven_Krishnaja_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idEleven_Krishnaja_TotalFund === totalAmount) {
+
+  idElevenShort.textContent = `${idEleven_Krishnaja_TotalFund}/- Full Paid`;
+  idElevenShort.style.color = 'darkcyan';
+
+} else {
+  idElevenShort.textContent = `${idElevenShortCalc}/-`;
+  idElevenShort.style.color = 'red';
+}
+
+
+
+// ID TWELVE 12 VISHNUJA
+const idTwelveName = document.getElementById('idTwelveName');
+const idTwelveShort = document.getElementById('idTwelveShort'); 
+
+idTwelveName.textContent = 'Vishnuja';
+
+const idTwelveShortCalc = totalAmount - idTwelve_Vishnuja_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idTwelve_Vishnuja_TotalFund === totalAmount) {
+
+  idTwelveShort.textContent = `${idTwelve_Vishnuja_TotalFund}/- Full Paid`;
+  idTwelveShort.style.color = 'darkcyan';
+
+} else {
+  idTwelveShort.textContent = `${idTwelveShortCalc}/-`;
+  idTwelveShort.style.color = 'red';
+}
+
+
+
+// ID THIRTEEN 13 MALU
+const idThirteenName = document.getElementById('idThirteenName');
+const idThirteenShort = document.getElementById('idThirteenShort'); 
+
+idThirteenName.textContent = 'Malu';
+
+const idThirteenShortCalc = totalAmount - idThirteen_Malu_TotalFund;//SHORTAGE CHECK CALCULATION.
+
+if (idThirteen_Malu_TotalFund === totalAmount) {
+
+  idThirteenShort.textContent = `${idThirteen_Malu_TotalFund}/- Full Paid`;
+  idThirteenShort.style.color = 'darkcyan';
+
+} else {
+  idThirteenShort.textContent = `${idThirteenShortCalc}/-`;
+  idThirteenShort.style.color = 'red';
+}
