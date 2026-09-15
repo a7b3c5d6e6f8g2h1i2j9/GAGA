ece23729c94d06fbd7ca782f573d15b4644a9db2
a7b3c5d6e6f8g2h1i2j9 <anirudhananu592@gmail.com>
2026-09-15 22:54:25 +0530

    add nishanth fund

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // ACCOUNT TOTAL AMOUNT DISPLAY
 
-let accountAmount = '3,000/-';
+let accountAmount = '3,500/-';
 
 const accountAmountDisplay = document.getElementById('accountAmountDisplay');
 
@@ -19,7 +19,7 @@ highlightDateDisplay.style.color = 'var(--highlighttextcolor)';
 
 const idOne_Adithyan_WeekFund = 100; 
 const idTwo_Jishnu_WeekFund = 100; 
-const idThree_Nishanth_WeekFund = 0; 
+const idThree_Nishanth_WeekFund = 500; 
 const idFour_Anirudhan_WeekFund = 100; 
 const idFive_Vasu_WeekFund = 0; 
 const idSix_Midhun_WeekFund = 100; 
