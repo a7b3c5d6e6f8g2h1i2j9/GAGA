9e71bcd34367803c08034c8dd16be3169cb26c5a
a7b3c5d6e6f8g2h1i2j9 <anirudhananu592@gmail.com>
2026-09-20 23:05:01 +0530

    fund updation

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ accountAmountDisplay.textContent = `Account : ₹${accountAmount}`;
 //HIGHLIGHT DATE DISPLAY
 const highlightDateDisplay = document.getElementById('highlightDateDisplay');
 
-highlightDateDisplay.textContent = `Sep : 12/9/26 - Saturday Fund`;
+highlightDateDisplay.textContent = `Sep : 19/9/26 - Saturday Fund`;
 highlightDateDisplay.style.color = 'var(--highlighttextcolor)';
 
 
@@ -22,7 +22,7 @@ const idTwo_Jishnu_WeekFund = 0;
 const idThree_Nishanth_WeekFund = 0; 
 const idFour_Anirudhan_WeekFund = 0; 
 const idFive_Vasu_WeekFund = 0; 
-const idSix_Midhun_WeekFund = 0; 
+const idSix_Midhun_WeekFund = 100; 
 const idSeven_Nkhil_WeekFund = 0; 
 const idEight_Jamshika_WeekFund = 0; 
 const idNine_Amika_WeekFund = 0; 
